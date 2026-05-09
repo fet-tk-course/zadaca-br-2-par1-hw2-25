501a5c80cb8a501e410c4419150047819f0b546d
MaidaKamencic <maida.kamencic@fet.ba>
2026-05-09 15:51:36 +0200

    Kreirana shema za kreiranje novog instrumenta

--- models_a.py
+++ models_a.py
@@ -4,4 +4,26 @@ from typing import Optional
 # TODO: Student A - Definiši svoj SQLModel entitet ovdje
 # 
 
+#Glavni model 
+class Instrument(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    inventory_number: str
+    manufacturer: str
+    purchase_year: int
+    price: float
+    is_available: bool
+    location: Optional[str] = None
+
+#Shema za kreiranje novog instrumenta
+class InstrumentCreate(SQLModel):
+    name: str
+    inventory_number: str
+    manufacturer: str
+    purchase_year: int
+    price: float
+    is_available: bool
+    location: Optional[str] = None
+
+
 
