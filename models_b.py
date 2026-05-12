3d3aece361f7eeb1fcfb1fece43239eff2ae8d8a
Amer_Imamović <amer.imamovic@fet.ba>
2026-05-12 18:48:10 +0200

    Dodane post i put metode

--- models_b.py
+++ models_b.py
@@ -9,3 +9,20 @@ class Measurement(SQLModel, table=True):
     required_accuracy: float
     description: Optional[str] = None
 
+
+
+class MeasurementCreate(SQLModel):
+    measurement_type: str
+    duration_minutes: int
+    is_automated: bool = False
+    required_accuracy: float
+    description: Optional[str] = None
+ 
+
+class MeasurementUpdate(SQLModel):
+    measurement_type: Optional[str] = None
+    duration_minutes: Optional[int] = None
+    is_automated: Optional[bool] = None
+    required_accuracy: Optional[float] = None
+    description: Optional[str] = None
+ 
--- routes_b.py
+++ routes_b.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
 
 from database import get_session
-from models_b import Measurement
+from models_b import Measurement, MeasurementCreate, MeasurementUpdate
 
 router = APIRouter(prefix="/measurements", tags=["Mensurations"])
 
@@ -19,3 +19,29 @@ def get_measurement(id: int, session: Session = Depends(get_session)):
     if not measurement:
         raise HTTPException(status_code=404, detail="Mjerenje nije pronađeno")
     return measurement
+
+
+
+@router.post("/", response_model=Measurement, status_code=201)
+def create_measurement(measurement_create: MeasurementCreate, session: Session = Depends(get_session)):
+    db_measurement = Measurement.model_validate(measurement_create)   
+    session.add(db_measurement)
+    session.commit()
+    session.refresh(db_measurement)
+    return db_measurement
+
+
+@router.put("/{id}", response_model=Measurement)
+def update_measurement(id: int, measurement_update: MeasurementCreate, session: Session = Depends(get_session)):
+    db_measurement = session.get(Measurement, id)
+    if not db_measurement:
+        raise HTTPException(status_code=404, detail="Mjerenje nije pronađeno")
+    measurement_data = measurement_update.model_dump()
+    for key, value in measurement_data.items():
+        setattr(db_measurement, key, value)
+    session.add(db_measurement)
+    session.commit()
+    session.refresh(db_measurement)
+    return db_measurement
+
+
